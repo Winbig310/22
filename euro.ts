ebac687f7b4c173ccec9006997b2501283811b79
Winbig310 <167060974+Winbig310@users.noreply.github.com>
2024-06-19 08:48:07 +0700

    Update euro.ts

--- euro.ts
+++ euro.ts
@@ -1,4 +1,4 @@
 #EXTM3U
-#EXTINF:-1,EURO tvg-logo="https://editorial.uefa.com/resources/0288-199734d7ac32-76cda290f242-1000/wtw_euro.jpeg"
+#EXTINF:-0 tvg-logo="https://editorial.uefa.com/resources/0288-199734d7ac32-76cda290f242-1000/wtw_euro.jpeg",EURO LIVE
 https://sgn.play-hometv.my.id:443/Sugeng/msY9yqKyrR/235.ts
 
